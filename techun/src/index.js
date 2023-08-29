64071be3f5bbefa3dcc03bd2caa2b61bf18fa848
Monu <monutanti123@gmail.com>
2023-08-29 10:47:47 +0530

    new branch is created

--- techun/src/index.js
+++ techun/src/index.js
@@ -1,13 +1,8 @@
 import ReactDom from "react-dom"; // this is the module 
 
                                     // which is used to fetch js to html.
-
-
-                                    import React from "react";
+import React from "react";
 import './index.css';        
-
 import App from './App';  // app jo rturn kar raha ha
-
-
 ReactDom.render(<App/>,document.getElementById('root'));
 
