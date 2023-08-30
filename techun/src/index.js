f840a5ee4bc32a3451d397e060c47bae2a08338c
Monu <monutanti123@gmail.com>
2023-08-30 13:59:29 +0530

    got it

--- techun/src/index.js
+++ techun/src/index.js
@@ -1,8 +1,13 @@
 import ReactDom from "react-dom"; // this is the module 
 
                                     // which is used to fetch js to html.
-import React from "react";
+
+
+                                    import React from "react";
 import './index.css';        
+
 import App from './App';  // app jo rturn kar raha ha
+
+
 ReactDom.render(<App/>,document.getElementById('root'));
 
