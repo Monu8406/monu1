d389d7e9c34cdf26779d8e2d36784b6fad5de7d6
Monu <monutanti123@gmail.com>
2023-08-31 04:29:45 +0530

    done2

--- techun/src/components/Expenses/ExpenseItem.js
+++ techun/src/components/Expenses/ExpenseItem.js
@@ -7,19 +7,21 @@ let ExpenseItem = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
-  return (
 
-   <Card className="expense-item">
+
+   return (
+      <Card className="expense-item">
       <div className="date">
         <div>{month}</div>
         <div>show</div>
+        <div></div>
         <div></div>
         <div>{props.date.getFullYear()}</div>
         <div>{day}</div>
        </div>
        <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${props.amount}1</div>
       </div>
       </Card>
      );
